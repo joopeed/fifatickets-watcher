b83661205e3f783001942e9292991f9b70832866
João Pedro Leôncio <joopeeds@gmail.com>
2014-07-01 18:21:42 -0300

    Ignoring all exceptions: FIFA system could be down

--- FIFAmonitor.py
+++ FIFAmonitor.py
@@ -2,13 +2,13 @@
 
 __author__ = 'João Pedro <joopeeds@gmail.com>'
 
-import urllib2, json, pynma, datetime
+import urllib2, json, pynma, datetime, time
 
 
 class MonitorFIFA():
     def __init__(self, winsound=False, android=False):
         self.game_code = "57"
-        self.mykey = 'mykey'
+        self.mykey = 'e25ab3fec01f5384bbee1af5d6ff9df3663df4d7a260d936'
         self.url = "https://fwctickets.fifa.com/TopsAkaCalls/Calls.aspx/getRefreshChartAvaDem?l=en&c=BRA"
         self.p = pynma.PyNMA(self.mykey)
         self.winsound = winsound
@@ -24,7 +24,7 @@ class MonitorFIFA():
         again = json.loads(resp_dic["d"]["data"])
         for each in again["BasicCodes"]["PRODUCTPRICES"]:
             if each["PRPProductId"] == "IMT" + self.game_code and each["PRPCategoryId"] in ["1", "2", "3", "4"] and \
-                            each["Quantity"] != "0":
+                            each["Quantity"] != "-1":
                 if self.winsound:
                     import winsound
                     winsound.Beep(2000, 5000)
@@ -34,7 +34,10 @@ class MonitorFIFA():
                 print "The tickets for the game %s are available right now. Run!" % self.game_code
 
 
-
 while True:
-    t = MonitorFIFA(winsound=True)
-    t.update()
+    try:
+        t = MonitorFIFA(winsound=True, android=True)
+        t.update()
+        time.sleep(1)
+    except:
+        print "Update - ERROR " + str(datetime.datetime.now())
